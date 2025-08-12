2071136cf1b4b99b4d2c1223f34fbd1b5c0781a3
kiranreddy444 <85559552+kiranreddy444@users.noreply.github.com>
2025-08-12 12:08:22 +0530

    Update HelloController.java

--- src/main/java/com/fatihhernn/ecommerce/entities/HelloController.java
+++ src/main/java/com/fatihhernn/ecommerce/entities/HelloController.java
@@ -1,4 +1,4 @@
-package com.example.demo.controller;
+package com.fatihhernn.ecommerce.entities;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String sayHello() {
-        return "Hello from Spring Boot!";
+        return "Hello from Spring Boot Docker";
     }
 }
